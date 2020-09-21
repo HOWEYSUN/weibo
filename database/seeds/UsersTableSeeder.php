ed52be9c869f486297a0cea165cb7498ec5d7609
HOWEYSUN <850732654@qq.com>
2020-09-21 07:21:52 +0000

    查看用户列表

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = factory(User::class)->times(50)->make();
+        User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
+
+        $user = User::find(1);
+        $user->name = 'howey';
+        $user->email = '850732654@qq.com';
+        $user->save();
+
+    }
+}
